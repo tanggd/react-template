a8a02274489da7a5903a64fff22117e407558af8
tanggd <tanggd11@foxmail.com>
2021-12-21 14:26:36 +0800

    lint

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -1,7 +1,11 @@
+import { Button } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
+
 export default function About() {
   return (
     <div>
       <h1>About</h1>
+      <Button type="primary" icon={<SearchOutlined />}>Button</Button>
     </div>
   )
 }
